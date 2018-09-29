976738dcf594246fb55fddf9db5e1e6d090c166b
toticavalcanti <toticavalcanti@hotmail.com>
2018-09-29 16:02:33 -0300

    generate forms/form-edit component

---
+++ src/app/forms/form-edit/form-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-edit',
+  templateUrl: './form-edit.component.html',
+  styleUrls: ['./form-edit.component.css']
+})
+export class FormEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/forms/forms.module.ts
+++ src/app/forms/forms.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormListComponent } from './form-list/form-list.component';
+import { FormEditComponent } from './form-edit/form-edit.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [FormListComponent]
+  declarations: [FormListComponent, FormEditComponent]
 })
 export class FormsModule { }
