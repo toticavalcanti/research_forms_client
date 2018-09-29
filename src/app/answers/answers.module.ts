53291c6767261e4902cd42533db2ec15971625fe
toticavalcanti <toticavalcanti@hotmail.com>
2018-09-29 16:17:38 -0300

    generate answers/shared/graph-number component

--- src/app/answers/answers.module.ts
+++ src/app/answers/answers.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AnswerListComponent } from './answer-list/answer-list.component';
 import { AnswerNewComponent } from './answer-new/answer-new.component';
+import { GraphNumberComponent } from './shared/graph-number/graph-number.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [AnswerListComponent, AnswerNewComponent]
+  declarations: [AnswerListComponent, AnswerNewComponent, GraphNumberComponent]
 })
 export class AnswersModule { }
