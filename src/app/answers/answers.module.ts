d3db7dc8eaf9e07acaf49462d36bdbffb2f90629
toticavalcanti <toticavalcanti@hotmail.com>
2018-09-29 16:20:30 -0300

    generate answers/shared/graph-boolean component

--- src/app/answers/answers.module.ts
+++ src/app/answers/answers.module.ts
@@ -4,11 +4,12 @@ import { AnswerListComponent } from './answer-list/answer-list.component';
 import { AnswerNewComponent } from './answer-new/answer-new.component';
 import { GraphNumberComponent } from './shared/graph-number/graph-number.component';
 import { GraphTextComponent } from './shared/graph-text/graph-text.component';
+import { GraphBooleanComponent } from './shared/graph-boolean/graph-boolean.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [AnswerListComponent, AnswerNewComponent, GraphNumberComponent, GraphTextComponent]
+  declarations: [AnswerListComponent, AnswerNewComponent, GraphNumberComponent, GraphTextComponent, GraphBooleanComponent]
 })
 export class AnswersModule { }
