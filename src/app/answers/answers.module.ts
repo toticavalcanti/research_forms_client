d9da7003a01e3a567b1739b5c3577d2dea9940e6
toticavalcanti <toticavalcanti@hotmail.com>
2018-09-29 16:12:45 -0300

    generate answers/answer-list component

--- src/app/answers/answers.module.ts
+++ src/app/answers/answers.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AnswerListComponent } from './answer-list/answer-list.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [AnswerListComponent]
 })
 export class AnswersModule { }
