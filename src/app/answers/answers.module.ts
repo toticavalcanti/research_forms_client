88d02eb57931867bdad46a5b6909ff645e1525b2
toticavalcanti <toticavalcanti@hotmail.com>
2018-09-29 16:19:09 -0300

    generate answers/shared/graph-text component

--- src/app/answers/answers.module.ts
+++ src/app/answers/answers.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { AnswerListComponent } from './answer-list/answer-list.component';
 import { AnswerNewComponent } from './answer-new/answer-new.component';
 import { GraphNumberComponent } from './shared/graph-number/graph-number.component';
+import { GraphTextComponent } from './shared/graph-text/graph-text.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [AnswerListComponent, AnswerNewComponent, GraphNumberComponent]
+  declarations: [AnswerListComponent, AnswerNewComponent, GraphNumberComponent, GraphTextComponent]
 })
 export class AnswersModule { }
---
+++ src/app/answers/shared/graph-text/graph-text.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-graph-text',
+  templateUrl: './graph-text.component.html',
+  styleUrls: ['./graph-text.component.css']
+})
+export class GraphTextComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
