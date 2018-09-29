b143e773bfe240bebe8636daa34e360cf876f73b
toticavalcanti <toticavalcanti@hotmail.com>
2018-09-29 16:23:41 -0300

    generate header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { MaterializeModule } from 'ng2-materialize';
 import { ColorPickerModule } from 'ngx-color-picker';
 import { ChartsModule } from 'ng2-charts';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
