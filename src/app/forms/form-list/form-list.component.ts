22c27bea6be31f2a90887fe6ba5aaa0053d16362
toticavalcanti <toticavalcanti@hotmail.com>
2018-09-29 16:00:01 -0300

    generate component forms/form-list

---
+++ src/app/forms/form-list/form-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-list',
+  templateUrl: './form-list.component.html',
+  styleUrls: ['./form-list.component.css']
+})
+export class FormListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/forms/forms.module.ts
+++ src/app/forms/forms.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormListComponent } from './form-list/form-list.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [FormListComponent]
 })
 export class FormsModule { }
