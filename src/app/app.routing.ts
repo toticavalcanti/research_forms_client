2d20801b94c3648fc946918f24ffde4035879d14
toticavalcanti <toticavalcanti@hotmail.com>
2018-10-10 20:15:11 -0300

    update src/app/app.routing.ts

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,15 +1,34 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
+ 
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { FormsModule } from './forms/forms.module';
 import { AnswersModule } from './answers/answers.module';
-
+import { UsersModule } from './users/users.module';
+ 
+ 
+import { Angular2TokenService } from 'angular2-token';
+import { FormListComponent } from './forms/form-list/form-list.component';
+import { FormEditComponent } from './forms/form-edit/form-edit.component';
+import { AnswerListComponent } from './answers/answer-list/answer-list.component';
+import { AnswerNewComponent } from './answers/answer-new/answer-new.component';
+import { LoginComponent } from './users/login/login.component';
+import { RegisterComponent } from './users/register/register.component';
+ 
+ 
+ 
 const appRoutes: Routes = [
     { path: '', pathMatch: 'full', component: HomeComponent },
     { path: 'not-found', component: NotFoundComponent },
-    { path: '**', redirectTo: 'not-found' }
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'forms', component: FormListComponent, canActivate: [Angular2TokenService]},
+    { path: 'forms/:id', component: FormEditComponent, canActivate: [Angular2TokenService]},
+    { path: 'forms/new', component: FormEditComponent, canActivate: [Angular2TokenService]},
+    { path: 'answers', component: AnswerNewComponent, canActivate: [Angular2TokenService]},
+    { path: 'answers/:id', component: AnswerListComponent, canActivate: [Angular2TokenService]},
+    { path: '**', component: NotFoundComponent },
 ];
-
+ 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
