23ca902d11f1caacc7febeab3ffa3ef9e3890699
toticavalcanti <toticavalcanti@hotmail.com>
2018-10-10 16:55:16 -0300

    generate component  users/register

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [LoginComponent]
+  declarations: [LoginComponent, RegisterComponent]
 })
 export class UsersModule { }
