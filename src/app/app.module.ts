e3964ffb1765ea2d1815a5dec11f5c4043ecd005
toticavalcanti <toticavalcanti@hotmail.com>
2018-10-10 19:59:49 -0300

    add imports UserModule and providers Angular2TokenService in src/app/app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FormsModule } from './forms/forms.module';
+import { UsersModule } from './users/users.module';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { AnswersModule } from './answers/answers.module';
+import { routing } from './app.routing';
+import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterializeModule } from 'ng2-materialize';
 import { ColorPickerModule } from 'ngx-color-picker';
 import { ChartsModule } from 'ng2-charts';
-import { HomeComponent } from './home/home.component';
-import { HeaderComponent } from './header/header.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { FormsModule } from './forms/forms.module';
-import { AnswersModule } from './answers/answers.module';
-import { RouterModule } from '@angular/router';
-import { routing } from './app.routing';
+import { HttpModule } from '@angular/http';
+import { Angular2TokenService } from 'angular2-token';
+
+
 
 
 @NgModule({
@@ -19,19 +25,21 @@ import { routing } from './app.routing';
     AppComponent,
     HomeComponent,
     HeaderComponent,
-    NotFoundComponent
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
+    RouterModule,
+    FormsModule,
+    AnswersModule,
+    UsersModule,
+    routing,
+    BrowserAnimationsModule,
     MaterializeModule.forRoot(),
     ColorPickerModule,
     ChartsModule,
-    FormsModule,
-    AnswersModule,
-    RouterModule,
-    routing
   ],
-  providers: [],
+  providers: [Angular2TokenService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
