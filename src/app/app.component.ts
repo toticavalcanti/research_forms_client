d839511859e3dd6c238458dce0b80191d9b98c9d
toticavalcanti <toticavalcanti@hotmail.com>
2018-10-10 20:07:28 -0300

    import Angular2TokenService in src/app/app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+	
 import { Component } from '@angular/core';
-
+import { Angular2TokenService } from 'angular2-token';
+ 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  constructor(private _tokenService: Angular2TokenService) {
+    this._tokenService.init({
+      apiPath: 'http://localhost:3000/api/v1',
+      signInRedirect: '/login',
+      globalOptions: {
+        headers: {
+          'Content-Type':     'application/json',
+          'Accept':           'application/json'
+        }
+      }
+    });
+  }
 }
