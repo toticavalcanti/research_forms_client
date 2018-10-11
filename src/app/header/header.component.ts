3028dc0379eace118cc4fe8122c19dd2a8d59442
toticavalcanti <toticavalcanti@hotmail.com>
2018-10-11 18:56:39 -0300

    insert import Angular2TokenService e MzToastService, create and set signOut method, and the costructor was modified

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Angular2TokenService } from 'angular2-token';
+import { MzToastService} from 'ng2-materialize';
+
 
 @Component({
   selector: 'app-header',
@@ -7,9 +10,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _tokenService: Angular2TokenService, private toastService: MzToastService) { }
 
   ngOnInit() {
   }
 
+  signOut() {
+    this._tokenService.signOut().subscribe(
+      res =>      { },
+      error =>    this.toastService.show('Error in Logout', 8000, 'red')
+    );
+  }
 }
