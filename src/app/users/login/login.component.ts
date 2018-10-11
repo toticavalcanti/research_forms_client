caeb6ad2da0e30330a3d72fbc357c460dd1c410e
toticavalcanti <toticavalcanti@hotmail.com>
2018-10-10 23:06:03 -0300

    set src/app/users/login/login.component.html

--- src/app/users/login/login.component.ts
+++ src/app/users/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Angular2TokenService, SignInData } from 'angular2-token';
+import { MzToastService} from 'ng2-materialize';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private _tokenService: Angular2TokenService,
+    private toastService: MzToastService,
+    private router: Router
+  ) { }
+
+  private _signInData: SignInData = <SignInData>{};
 
   ngOnInit() {
   }
 
+  onSubmit() {
+    this._tokenService.signIn(this._signInData).subscribe(
+      res => {
+        this.router.navigate(['/forms']);
+      }, error => {
+        this._signInData = <SignInData>{};
+        console.log(error._body);
+        if ( error.status !== 0 ) {
+          for (const message of JSON.parse(error._body)['errors']) {
+            this.toastService.show(message, 8000, 'red');
+          }
+        } else {
+          this.toastService.show('Connection Error', 8000, 'red');
+        }
+      }
+    );
+  }
 }
