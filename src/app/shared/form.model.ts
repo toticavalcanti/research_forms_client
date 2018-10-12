b28e0881633d76dd1315dffadcc675ccf0fa4f99
toticavalcanti <toticavalcanti@hotmail.com>
2018-10-11 23:15:05 -0300

    set src/app/shared/form.model.ts

---
+++ src/app/shared/form.model.ts
@@ -0,0 +1,30 @@
+import { Question } from './question.model';
+ 
+export class Form {
+    id: number;
+    title: string;
+    description: string;
+    questions: Question[] = [];
+    user_id: number;
+    enable: boolean;
+    primary_color: string;
+    slug: string;
+ 
+    constructor(formInfo: any) {
+        this.id = formInfo.id;
+        this.title = formInfo.title;
+        this.description = formInfo.description;
+        this.user_id = formInfo.user_id;
+        this.enable = formInfo.enable;
+        this.primary_color = formInfo.primary_color;
+        this.slug = formInfo.slug;
+ 
+        this.includeQuestions(formInfo.questions);
+    }
+ 
+    private includeQuestions(questions: any) {
+        for (const count in questions) {
+            this.questions.push( new Question(questions[count]) );
+        }
+    }
+}
