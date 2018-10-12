d2d3424c86a862b01a08386b5b4579a4f7f2df0b
toticavalcanti <toticavalcanti@hotmail.com>
2018-10-12 12:28:16 -0300

    update src/app/shared/answer.service.ts

--- src/app/shared/answer.service.ts
+++ src/app/shared/answer.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@angular/core';
-
+import { Http } from '@angular/http';
+import { Angular2TokenService, A2tUiModule} from 'angular2-token';
+ 
 @Injectable()
 export class AnswerService {
-
-  constructor() { }
-
+ 
+  constructor(private http: Http, private _tokenService: Angular2TokenService) { }
+ 
+  getAnswers(form_id) {
+    return this._tokenService.get('answers', {search: {form_id: form_id}}).map(res =&gt; res.json());
+  }
+ 
+  getAnswer(answer_id) {
+    return this._tokenService.get('answers/' + answer_id).map(res =&gt; res.json());
+  }
+ 
+  createAnswer(answer_params) {
+    return this._tokenService.post('answers', answer_params).map(res =&gt; res.json());
+  }
+ 
+  destroyAnswer(answer_id) {
+    return this._tokenService.delete('answers/' + answer_id).map(res =&gt; res.json());
+  }
 }
