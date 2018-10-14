af2074060d086f86d7bc7491500d777f2976ff67
toticavalcanti <toticavalcanti@hotmail.com>
2018-10-14 13:46:27 -0300

    fix src/app/shared/question.service.ts

--- src/app/shared/question.service.ts
+++ src/app/shared/question.service.ts
@@ -1,21 +1,21 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import { Angular2TokenService } from 'angular2-token';
- 
+
 @Injectable()
 export class QuestionService {
- 
+
   constructor(private http: Http, private _tokenService: Angular2TokenService) { }
- 
+
   createQuestion(form_id, question) {
-    return this._tokenService.post('questions', {form_id: form_id, question}).map(res =&gt; res.json());
+    return this._tokenService.post('questions', {form_id: form_id, question}).map(res => res.json());
   }
- 
+
   updateQuestion(question_id, question_params) {
-    return this._tokenService.put('questions/' + question_id, question_params).map(res =&gt; res.json());
+    return this._tokenService.put('questions/' + question_id, question_params).map(res => res.json());
   }
- 
+
   destroyQuestion(question_id) {
-    return this._tokenService.delete('questions/' + question_id).map(res =&gt; res.json());
+    return this._tokenService.delete('questions/' + question_id).map(res => res.json());
   }
 }
