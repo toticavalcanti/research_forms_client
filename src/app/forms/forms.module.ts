2dfa578794e26250f7533d635203312d11e07b52
toticavalcanti <toticavalcanti@hotmail.com>
2018-10-14 13:17:16 -0300

    update src/app/forms/forms.module.ts

--- src/app/forms/forms.module.ts
+++ src/app/forms/forms.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormListComponent } from './form-list/form-list.component';
 import { FormEditComponent } from './form-edit/form-edit.component';
-//import { ActiveComponent } from './shared/active/active.component';
+import { ActiveComponent } from './shared/active/active.component';
 import { MaterializeModule } from 'ng2-materialize';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
