71109bf66aaec09fdad87c81e45ea1b97fa4d706
toticavalcanti <toticavalcanti@hotmail.com>
2018-10-14 13:13:53 -0300

    set src/app/forms/form-list/form-list.component.ts

--- src/app/forms/form-list/form-list.component.ts
+++ src/app/forms/form-list/form-list.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormService } from '../../shared/form.service';
+import { MzToastService} from 'ng2-materialize';
+
+import { Form } from '../../shared/form.model';
+import { Question } from '../../shared/question.model';
 
 @Component({
   selector: 'app-form-list',
@@ -7,9 +12,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormListComponent implements OnInit {
 
-  constructor() { }
+  private forms: Form[] = [];
+
+  constructor(private formService: FormService, private toastService: MzToastService) { }
+
 
   ngOnInit() {
+    this.formService.getForms().subscribe(data => {
+      for (const form of data){
+        this.forms.push(new Form(form));
+      }
+    });
   }
 
+  deleteForm(form): boolean {
+    if (confirm('Your want delete this form?')) {
+      this.formService.destroyForm(form.id).subscribe(data => {
+        const index = this.forms.indexOf(form);
+        this.forms.splice(index, 1);
+        this.toastService.show('Form deleted', 8000, 'green');
+      }, error => {
+        this.toastService.show('Error in delete Form ' + form.title, 8000, 'red');
+      });
+    }
+    return false;
+  }
 }
