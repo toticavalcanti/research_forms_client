1a2e06ab99f2438acb45040a9c8110cab4a0f77d
toticavalcanti <toticavalcanti@hotmail.com>
2018-10-14 13:15:48 -0300

    update src/app/forms/forms.module.ts

--- src/app/forms/forms.module.ts
+++ src/app/forms/forms.module.ts
@@ -3,10 +3,21 @@ import { CommonModule } from '@angular/common';
 import { FormListComponent } from './form-list/form-list.component';
 import { FormEditComponent } from './form-edit/form-edit.component';
 import { ActiveComponent } from './shared/active/active.component';
+import { MaterializeModule } from 'ng2-materialize';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { routing } from './../app.routing';
+import { FormsModule as CoreFormsModule } from '@angular/forms';
+
+
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    BrowserAnimationsModule,
+    MaterializeModule.forRoot(),
+    routing,
+    CoreFormsModule
   ],
   exports: [
     FormListComponent
