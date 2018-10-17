58c31ac4a3e3f2b2e2f8092e76a88a425b5629b0
toticavalcanti <toticavalcanti@hotmail.com>
2018-10-17 00:27:20 -0300

    update src/app/forms/shared/active/active.component.ts

--- src/app/forms/shared/active/active.component.ts
+++ src/app/forms/shared/active/active.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormService } from '../../../shared/form.service';
+import { MzToastService} from 'ng2-materialize';
+import { Form } from '../../../shared/form.model';
 
 @Component({
   selector: 'app-active',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActiveComponent implements OnInit {
 
-  constructor() { }
+  @Input() form: Form;
+
+  constructor(private formService: FormService, private toastService: MzToastService) { }
 
   ngOnInit() {
   }
 
+  toggleEnable() {
+    this.formService.updateForm(this.form.id, this.form).subscribe(data => {
+      if (this.form.enable) {
+        this.toastService.show('Form enabled', 8000, 'green');
+      } else {
+        this.toastService.show('Form disabled', 8000, 'green');
+      }
+    }, error => {
+      if (this.form.enable) {
+        this.toastService.show('Error in enable Form', 8000, 'red');
+      } else {
+        this.toastService.show('Error in disable Form', 8000, 'red');
+      }
+    });
+  }
 }
