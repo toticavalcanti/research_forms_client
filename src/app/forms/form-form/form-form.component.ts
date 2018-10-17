b0a5af14437d19e275b6f59ae41eb0d8be8df60a
toticavalcanti <toticavalcanti@hotmail.com>
2018-10-17 15:28:18 -0300

    merge with form_edit_component branch

---
+++ src/app/forms/form-form/form-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-form',
+  templateUrl: './form-form.component.html',
+  styleUrls: ['./form-form.component.css']
+})
+export class FormFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/forms/question-form/question-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-question-form',
+  templateUrl: './question-form.component.html',
+  styleUrls: ['./question-form.component.css']
+})
+export class QuestionFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
