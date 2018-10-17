f02855a844a0690b4ca3be077ee5fd6db06ee75a
toticavalcanti <toticavalcanti@hotmail.com>
2018-10-17 15:32:45 -0300

    set src/app/forms/form-form/form-form.component.ts

--- src/app/forms/form-form/form-form.component.ts
+++ src/app/forms/form-form/form-form.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input} from '@angular/core';
+
+import { FormService } from '../../shared/form.service';
+import { Form } from '../../shared/form.model';
+import { MzToastService} from 'ng2-materialize';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-form-form',
@@ -7,9 +13,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormFormComponent implements OnInit {
 
-  constructor() { }
+  @Input() form: Form;
 
-  ngOnInit() {
+  constructor(
+              private formService: FormService,
+              private toastService: MzToastService,
+              private router: Router
+              ) { }
+
+  ngOnInit() { }
+
+  onSubmit(f) {
+    if (this.form.id) {
+      this.formService.updateForm(this.form.id, this.form).subscribe(data => {
+        this.toastService.show('Form updated', 8000, 'green');
+      }, error => {
+        this.toastService.show('Problem in Form creation', 8000, 'red');
+      });
+    } else {
+      this.formService.createForm(this.form).subscribe(data => {
+        this.router.navigate(['/forms/' + data.slug]);
+      }, error => {
+        this.toastService.show('Problem in Form creation', 8000, 'red');
+      });
+    }
   }
-
 }
