c23b93f20b756ec33dc4b98d136db466c263bb3f
toticavalcanti <toticavalcanti@hotmail.com>
2018-10-18 14:47:40 -0300

    fix src/app/forms/question-list/question-list.component.ts

--- src/app/forms/question-list/question-list.component.ts
+++ src/app/forms/question-list/question-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { QuestionService } from '../../shared/question.service';
+import { Question } from '../../shared/question.model';
 
 @Component({
   selector: 'app-question-list',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuestionListComponent implements OnInit {
 
+  @Input() questions: Question[];
+  @Input() form_id: number;
+  public question_new: Question = new Question({});
+
   constructor() { }
 
   ngOnInit() {
   }
-
 }
