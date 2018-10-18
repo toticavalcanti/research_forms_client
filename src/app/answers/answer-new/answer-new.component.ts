2c8e93eec4403bd95106abe22bef63eb6845824b
toticavalcanti <toticavalcanti@hotmail.com>
2018-10-18 15:05:34 -0300

    set src/app/answers/answer-new/answer-new.component.ts

--- src/app/answers/answer-new/answer-new.component.ts
+++ src/app/answers/answer-new/answer-new.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { FormService } from '../../shared/form.service';
+import { MzToastService} from 'ng2-materialize';
+import { Form } from '../../shared/form.model';
+import { QuestionService } from '../../shared/question.service';
+import { Question } from '../../shared/question.model';
+import { Answer } from '../../shared/answer.model';
+import { AnswerService } from '../../shared/answer.service';
+import { QuestionsAnswer } from '../../shared/questions_answer.model';
+import { Router, ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-answer-new',
@@ -7,9 +17,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AnswerNewComponent implements OnInit {
 
-  constructor() { }
+  public form: Form = new Form({});
+  public answer: Answer;
+  public questions_answer: QuestionsAnswer;
+
+
+  constructor(
+    private formService: FormService,
+    private toastService: MzToastService,
+    private questionService: QuestionService,
+    private answerService: AnswerService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
+
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      if (params['id'] !== undefined) {
+        this.formService.getForm(params['id']).subscribe(data => {
+          this.form = new Form(data);
+          this.answer = new Answer({form_id: this.form.id});
+          for (const question of this.form.questions) {
+            this.answer.questions_answers.push(new QuestionsAnswer({question: question}));
+          }
+        });
+      }
+    });
   }
 
+  onSubmit(f) {
+    console.log(this.answer);
+    this.answerService.createAnswer(this.answer).subscribe(data => {
+      this.toastService.show('Answer send with sucess', 10000, 'green');
+      this.answer = new Answer({form_id: this.form.id});
+      for (const question of this.form.questions) {
+        this.answer.questions_answers.push(new QuestionsAnswer({question: question}));
+      }
+    }, error => {
+      this.toastService.show('Problem in send answer', 8000, 'red');
+    });
+  }
 }
