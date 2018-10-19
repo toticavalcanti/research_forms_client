037c42925bd11e59a5c71c4de730ad1f6be6a054
toticavalcanti <toticavalcanti@hotmail.com>
2018-10-19 15:41:23 -0300

    update src/app/answers/answer-list/all/all.component.ts

--- src/app/answers/answer-list/all/all.component.ts
+++ src/app/answers/answer-list/all/all.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Question } from '../../../shared/question.model';
+import { AnswerService } from '../../../shared/answer.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-all',
@@ -7,9 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AllComponent implements OnInit {
 
-  constructor() { }
+  public questions: Question[] = [];
+
+  constructor(
+    private answerService: AnswerService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      if (params['id'] !== undefined) {
+        this.answerService.getAnswers(params['id']).subscribe(data => {
+          for (const question of data) {
+            this.questions.push(new Question(question));
+          }
+        });
+      }
+    });
   }
-
 }
