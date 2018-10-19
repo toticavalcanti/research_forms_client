0a5b22ae356cbde463e2f3c70f21d9e103f62aa8
toticavalcanti <toticavalcanti@hotmail.com>
2018-10-19 16:48:43 -0300

    update src/app/answers/shared/graph-boolean/graph-boolean.component.ts

--- src/app/answers/shared/graph-boolean/graph-boolean.component.ts
+++ src/app/answers/shared/graph-boolean/graph-boolean.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { QuestionsAnswer } from '../../../shared/questions_answer.model';
 
 @Component({
   selector: 'app-graph-boolean',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GraphBooleanComponent implements OnInit {
 
+  @Input() questions_answers: QuestionsAnswer[];
+  public doughnutChartLabels: string[];
+  public doughnutChartData: number[];
+  public doughnutChartType = 'doughnut';
+
   constructor() { }
 
   ngOnInit() {
+    let yes = 0;
+    let no  = 0;
+    for (const qa of this.questions_answers) {
+      if (qa.value != null) {
+        if (qa.value === '0') {
+          no += 1;
+        }else {
+          yes += 1;
+        }
+      }
+    }
+    if (yes > 0 || no > 0) {
+      this.doughnutChartLabels = ['no', 'yes'];
+      this.doughnutChartData = [no, yes];
+    }
   }
-
 }
