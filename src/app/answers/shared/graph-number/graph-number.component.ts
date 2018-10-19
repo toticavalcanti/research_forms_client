fe6cad6b46e9941e114bc23e3abaf3a8dc338f39
toticavalcanti <toticavalcanti@hotmail.com>
2018-10-19 16:46:04 -0300

    set src/app/answers/shared/graph-number/graph-number.component.ts

--- src/app/answers/shared/graph-number/graph-number.component.ts
+++ src/app/answers/shared/graph-number/graph-number.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { QuestionsAnswer } from '../../../shared/questions_answer.model';
 
 @Component({
   selector: 'app-graph-number',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GraphNumberComponent implements OnInit {
 
+  @Input() questions_answers: QuestionsAnswer[];
+  public pieChartLabels: string[];
+  public pieChartData: number[];
+  public pieChartType = 'pie';
+
   constructor() { }
 
   ngOnInit() {
-  }
+    const answers = {};
 
+    for (const qa of this.questions_answers) {
+      if (qa.value != null) {
+        if (answers.hasOwnProperty(qa.value) !== true) {
+          answers[qa.value] = 1;
+        } else {
+          answers[qa.value] += 1;
+        }
+      }
+    }
+
+    const answers_labels = [];
+    const answers_values = [];
+    for (const key in answers) {
+      if (answers.hasOwnProperty(key)) {
+        answers_labels.push(key);
+        answers_values.push(answers[key]);
+      }
+    }
+    this.pieChartLabels = answers_labels;
+    this.pieChartData = answers_values;
+  }
 }
