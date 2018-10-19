dbfbed30d61e07abe9d749b14730e0fe624c5496
toticavalcanti <toticavalcanti@hotmail.com>
2018-10-19 15:15:19 -0300

    generate component answers/answer-list/all

--- src/app/answers/answers.module.ts
+++ src/app/answers/answers.module.ts
@@ -7,12 +7,13 @@ import { GraphTextComponent } from './shared/graph-text/graph-text.component';
 import { GraphBooleanComponent } from './shared/graph-boolean/graph-boolean.component';
 import { IndividualDetailsComponent } from './shared/individual-details/individual-details.component';
 import { FormsModule as CoreFormsModule } from '@angular/forms';
+import { AllComponent } from './answer-list/all/all.component';
 
 @NgModule({
   imports: [
     CommonModule,
     CoreFormsModule
   ],
-  declarations: [AnswerListComponent, AnswerNewComponent, GraphNumberComponent, GraphTextComponent, GraphBooleanComponent, IndividualDetailsComponent]
+  declarations: [AnswerListComponent, AnswerNewComponent, GraphNumberComponent, GraphTextComponent, GraphBooleanComponent, IndividualDetailsComponent, AllComponent]
 })
 export class AnswersModule { }
