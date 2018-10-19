37155dd1ed546ee4d9c64e6fe6e1fa864844344a
toticavalcanti <toticavalcanti@hotmail.com>
2018-10-19 15:58:36 -0300

    update src/app/answers/shared/graph-text/graph-text.component.ts

--- src/app/answers/shared/graph-text/graph-text.component.ts
+++ src/app/answers/shared/graph-text/graph-text.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { QuestionsAnswer } from '../../../shared/questions_answer.model';
 
 @Component({
   selector: 'app-graph-text',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GraphTextComponent implements OnInit {
 
+  @Input() questions_answers: QuestionsAnswer[];
+  public answers = [];
+
   constructor() { }
 
   ngOnInit() {
+    for (const qa of this.questions_answers) {
+      if (qa.value != null) {
+        this.answers.push(qa.value);
+      }
+    }
   }
-
 }
