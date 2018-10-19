e830de28c916f4080215a76140ecb0bbfef21d69
toticavalcanti <toticavalcanti@hotmail.com>
2018-10-19 15:34:22 -0300

    update src/app/answers/answers.module.ts

--- src/app/answers/answers.module.ts
+++ src/app/answers/answers.module.ts
@@ -1,20 +1,34 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AnswerListComponent } from './answer-list/answer-list.component';
-import { AnswerNewComponent } from './answer-new/answer-new.component';
 import { GraphNumberComponent } from './shared/graph-number/graph-number.component';
-import { GraphTextComponent } from './shared/graph-text/graph-text.component';
 import { GraphBooleanComponent } from './shared/graph-boolean/graph-boolean.component';
 import { IndividualDetailsComponent } from './shared/individual-details/individual-details.component';
+import { GraphTextComponent } from './shared/graph-text/graph-text.component';
+import { AnswerNewComponent } from './answer-new/answer-new.component';
 import { FormsModule as CoreFormsModule } from '@angular/forms';
+import { MaterializeModule } from 'ng2-materialize';
 import { AllComponent } from './answer-list/all/all.component';
 import { IndividualComponent } from './answer-list/individual/individual.component';
+import { ChartsModule } from 'ng2-charts/ng2-charts';
+
 
 @NgModule({
   imports: [
     CommonModule,
-    CoreFormsModule
+    CoreFormsModule,
+    MaterializeModule.forRoot(),
+    ChartsModule
   ],
-  declarations: [AnswerListComponent, AnswerNewComponent, GraphNumberComponent, GraphTextComponent, GraphBooleanComponent, IndividualDetailsComponent, AllComponent, IndividualComponent]
+  declarations: [
+    AnswerListComponent,
+    GraphNumberComponent,
+    GraphBooleanComponent,
+    IndividualDetailsComponent,
+    GraphTextComponent,
+    AnswerNewComponent,
+    AllComponent,
+    IndividualComponent
+  ]
 })
 export class AnswersModule { }
